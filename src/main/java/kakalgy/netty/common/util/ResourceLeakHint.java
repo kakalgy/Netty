2218eb5e7c952cb63b234d87fe2c3c704327d498
kakalgy <kaka_lgy@163.com>
2017-02-09 17:04:01 +0800

    Resource

--- src/main/java/kakalgy/netty/common/util/ResourceLeakHint.java
+++ src/main/java/kakalgy/netty/common/util/ResourceLeakHint.java
@@ -1,5 +1,13 @@
 package kakalgy.netty.common.util;
 
+/**
+ * 返回一个使人可以容易阅读的信息来追踪泄露的地方 </br></br> A hint object that provides human-readable
+ * message for easier resource leak tracking.
+ */
 public interface ResourceLeakHint {
-
+	/**
+	 * 返回一个使人可以容易阅读的信息来追踪泄露的地方 </br></br> Returns a human-readable message that
+	 * potentially enables easier resource leak tracking.
+	 */
+	String toHintString();
 }
